f74d8297f34489e0c8f8fbcf1853cd8ead362a60
Manant243 <manant.tyagi2020@vitbhopal.ac.in>
2023-04-14 22:22:16 +0530

    User profile creation

---
+++ backend/controllers/onboarding.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const User = require('../models/User.model');
+const Profile = require('../models/Profile.model');
+const Follower = require('../models/Follower.model');
+const Notification = require('../models/Notification.model');
+const Chat = require('../models/Chat.model');
+
+const upload = require('../middleware/imageUpload.middleware');
+
+// @route:  POST /api/onboarding/:token
+router.post('/:token', upload.single('profilePic'), async (req, res) => {
+    const { token } = req.params;
+    const { bio, techStack, social } = req.body;
+    const { github, linkedin, website, twitter, instagram, youtube } = JSON.parse(social);
+
+    const verificationToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    try {
+        const user = await User.findOne({ verificationToken });
+        if(!user){
+            return res.status(400).json({
+                success : false,
+                message : 'Invalid or expired Token',
+            });
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+
+        if(req.file){
+            user.profilePicUrl = req.file.path;
+        }
+
+        await user.save();
+
+        let profileFields = {};
+        profileFields.user = user._id;
+        profileFields.bio = bio;
+        profileFields.techStack = JSON.parse(techStack);
+
+        profileFields.social = {};
+        if (github) profileFields.social.github = github;
+        if (website) profileFields.social.website = website;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (instagram) profileFields.social.instagram = instagram;
+        if (youtube) profileFields.social.youtube = youtube;
+
+        await new Profile(profileFields).save();
+
+        await new Follower({ user : user._id, followers : [], following : [] }).save();
+
+        jwt.sign({ userId : user._id}, process.env.JWT_SECRET, (err, token) => {
+            if(err) throw err;
+            res.status(200).json({
+                success : true,
+                message : 'User verified and onboarded',
+                token,
+            });
+        })
+    }   
+    catch (err){
+        console.error(err);
+        res.status(500).json({
+            success : false,
+            message : 'Server error',
+        });
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
-const app = require('./app.js');
+
+const app = express();
+app.use(express.json());
 
 mongoose.set('strictQuery', true);
 
