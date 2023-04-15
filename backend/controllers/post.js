308d172540a0f0c2ffe66de40b99b95867fcc702
Manant243 <manant.tyagi2020@vitbhopal.ac.in>
2023-04-15 10:18:49 +0530

    Adding a post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,256 +1,50 @@
-const Post = require("../models/Post");
-const User = require("../models/User");
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
 
-exports.createPost = async (req, res) => {
+const Post = require('../models/Post.model');
+const User = require('../models/User.model');
+const Follower = require('../models/Follower.model');
+const Comment = require('../models/Comment.model');
 
-    try{
-        const newPostData = {
-            caption : req.body.caption,
+const auth = require('../middleware/auth.middleware');
+const upload = require('../middleware/imageUpload.middleware');
 
-            image : {
-                public_id : "req.body.public_id",
-                url : "req.body.url"
-            },
+// @route   POST /api/posts
+// create a new post 
+router.post('/', auth, upload.array('images', 5), async (req, res) => {
+    const { title, description, liveDemo, sourceCode, techStack } = req.body;
 
-            owner : req.user._id
-        }
-        
-        const newPost = await Post.create(newPostData);
-        
-        const user = await User.findById(req.user._id);
-
-        user.posts.push(newPost._id);
-
-        await user.save();
-
-        res.status(201).json({
-            success : true,
-            post : newPost  
-        });
-
-    }
-    catch(error){
-        res.status(500).json({
+    if(req.files.length < 1){
+        return res.status(400).json({
             success : false,
-            message : error.message
+            message : 'Atleast one image is required',
         });
     }
-    
-};
-
-exports.deletePost = async (req, res) => {
 
     try {
-        const post = await Post.findById(req.params.id);
+        const postObj = {
+            user : req.userId,
+            title,
+            description,
+            images : req.files.map((file) => file.path),
+            liveDemo,
+            techStack : JSON.parse(techStack),
+        };
 
-        if(!post){
-            return res.status(404).json({
-                success : false,
-                message : "Post not found"
-            });
-        }
+        if (sourceCode) postObj.sourceCode = sourceCode;
 
+        const post = await new Post(postObj).save();
+        await new Comment({ post : post._id, comments : [] }).save();
 
-        if(post.owner.toString() !== req.user._id.toString()){
-            return res.status(401).json({
-                success : false,
-                error : "Unauthorized access"
-            });
-        }
-
-        await Post.findOneAndRemove({_id : post});
-
-        const user = await User.findById(req.user._id);
-        const index = await user.posts.indexOf(req.params.id);
-
-        user.posts.splice(index, 1);
-        await user.save();
-
-        return res.status(200).json({
-            success : true,
-            message : "Post Deleted"
-        });
-
+        res.status(201).json(post);
     }
-    catch (error){
-        return res.status(500).json({
-            success : false,
-            message : error.message
-        });
-    }
-
-}
-
-exports.updatePost = async (req, res) => {
-    try{
-
-        const post = await Post.findById(req.params.id);
-
-        if(!post){
-            return res.status(404).json({
-                success : false,
-                message : "Post not found"
-            });
-        }
-
-        if(post.owner.toString() !== req.user._id.toString()){
-            return res.status(401).json({
-                success : false,
-                message : "Unauthorized"
-            });
-        }
-
-        post.caption = req.body.caption;
-        await post.save();
-
-        return res.status(200).json({
-            success : true,
-            message : "Post Updated"
-        });
-
-    }
-    catch (error){
-        return res.status(500).json({
-            success : false,
-            message : error.message
-        });
-    }
-}
-
-exports.likeAndUnlikePost = async (req, res) => {
-    
-    try{
-        const post = await Post.findById(req.params.id);
-
-        if(!post){
-            return res.status(404).json({
-                success : false,
-                message : "Post not found"
-            });
-        }
-
-        let exists = post.likes.includes(req.user._id);
-
-        if(exists){
-
-            const index = post.likes.indexOf(req.user._id); 
-            post.likes.splice(index, 1);
-
-            await post.save();
-
-            return res.status(200).json({
-                success : true,
-                message : "Post Unliked"
-            });
-        }
-        else{
-
-            post.likes.push(req.user._id);
-            await post.save();
-
-            return res.status(200).json({
-                success : true,
-                message : "Post liked"
-            });
-        }
-
-    }
-    catch (error){
+    catch (err){
+        console.error(err);
         res.status(500).json({
             success : false,
-            message : error.message
+            message : 'Server error',
         });
     }
-}
+});
 
-exports.addComment = async(req, res) => {
-    try{
-
-        const post = await Post.findById(req.params.id);
-
-        if(!post){
-            return res.status(404).json({
-                success : false,
-                message : "Post not found"
-            });
-        }
-
-        post.comments.push({
-            user : req.user._id,
-            comment : req.body.comment
-        });
-
-        await post.save();
-
-        return res.status(200).json({
-            success : true,
-            message : "Comment Added"
-        });
-
-    }
-    catch (error){
-        return res.status(500).json({
-            success : false,
-            error : error.message
-        });
-    }
-}
-
-exports.deleteComment = async(req, res) => {
-    try{
-
-        const post = await Post.findById(req.params.id);
-
-        if(!post){
-            return res.status(404).json({
-                success : false,
-                message : "Post not found"
-            });
-        }
-
-        if(post.owner.toString() === req.user._id.toString()){
-
-            if(req.body.commentId == undefined){
-                return res.status(400).json({
-                    success : false,
-                    message : "Comment Id is required"
-                });
-            }
-
-            post.comments.forEach((item, index) => {
-                if(item._id.toString() === req.body.commentId.toString()){
-                    return post.comments.splice(index, 1);
-                }
-            });
-
-            await post.save();
-
-            return res.status(200).json({
-                success : true,
-                message : "Comment Deleted"
-            });
-
-        }
-        else{
-            post.comments.forEach((item, index) => {
-                if(item.user.toString() === req.user._id.toString()){
-                    return post.comments.splice(index, 1);
-                }
-            });
-
-            await post.save();
-
-            res.status(200).json({
-                success : true,
-                message : "Comment Deleted"
-            });
-        }
-
-    }
-    catch (error){
-        return res.status(200).json({
-            success : true,
-            message : "Post Added"
-        });
-    }
-}
