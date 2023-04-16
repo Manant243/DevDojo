8465a9e454ec6bced94dcf4eaf1758a646b49479
Manant243 <manant.tyagi2020@vitbhopal.ac.in>
2023-04-16 14:08:44 +0530

    Adding a comment

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -34,3 +34,48 @@ router.get('/:postId', async (req, res) => {
     }
 });
 
+// @route   POST /api/comments/:postId
+// @desc    Add a new comment to post
+router.post('/:postId', auth, async (req, res) => {
+    try{
+        if(req.body.text.length < 1){
+            return res.status(400).json({
+                success : false,
+                message : 'Comment must be atleast 1 character long',
+            });
+        }
+
+        let post = await Comment.findOne({ post : req.params.postId });
+        if(!post){
+            return res.status(404).json({
+                success : false,
+                message : 'Post not found',
+            });
+        }
+
+        const comment = {
+            _id : uuid(),
+            user : req.userId,
+            text : req.body.text,
+            date : Date.now(),
+            likes : [],
+            replies : [],
+        };
+
+        post.comments.unshift(comment);
+        post = await post.save();
+
+        post = await Comment.populate(post, 'comments.user');
+        post = await Comment.populate(post, 'comments.replies.user');
+
+        res.status(201).json(post.comments); 
+    }
+    catch (error){
+        console.error(error);
+        res.status(500).json({
+            success : false,
+            message : 'Server error',
+        });
+    }
+});
+
