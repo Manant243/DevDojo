8fdf734bf28ae40d0a41dabc9ddabb8018ab792d
Manant243 <manant.tyagi2020@vitbhopal.ac.in>
2023-04-17 15:51:00 +0530

    Sending a message

--- backend/server-utils/chat.js
+++ backend/server-utils/chat.js
@@ -17,5 +17,34 @@ const loadMessages = async (userId, messagesWith) => {
 
         return { chat };
     }
+    catch (error){
+        console.error(error);
+        return { error };
+    }
 };
+
+const sendMessage = async (userId, receiverId, message) => {
+    try {
+        const user = await Chat.findOne({ user : userId });
+        const receiver = await Chat.findOne({ user : receiverId });
+
+        const newMessage = {
+            sender : userId,
+            receiver : receiverId,
+            message,
+            date : Date.now(),
+        };
+
+        await addChat(user, receiverId, newMessage);
+        await addChat(receiver, userId, newMessage);
+
+        return { newMessage };
+    }
+    catch (error){
+        console.error(error);
+        return { error };
+    }
+};
+
+
 
