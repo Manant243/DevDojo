a79a26a446b1430b0a2d05060c34b4f46fa70e15
Manant243 <manant.tyagi2020@vitbhopal.ac.in>
2023-04-18 14:05:14 +0530

    Sockets

---
+++ backend/server-utils/sockets.js
@@ -0,0 +1,31 @@
+const users = [];
+
+const addUser = (userId, socketId) => {
+  const user = users.find((user) => user.userId === userId);
+
+  if (user && user.socketId === socketId) {
+    return users;
+  } 
+  else {
+    if (user && user.socketId !== socketId) {
+      removeUser(user.socketId);
+    }
+    const newUser = { userId, socketId };
+    users.push(newUser);
+
+    return users;
+  }
+};
+
+const removeUser = (socketId) => {
+  const index = users.map((user) => user.socketId).indexOf(socketId);
+  users.splice(index, 1);
+  
+  return users;
+};
+
+const findConnectedUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+module.exports = { addUser, removeUser, findConnectedUser };
